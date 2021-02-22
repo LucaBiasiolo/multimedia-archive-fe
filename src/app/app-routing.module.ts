84f7d92860c756f2581528c111088fadd8cba6e0
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-22 13:01:43 +0100

    Create route per nuovi componenti

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArchiveDetailComponent } from './archive-detail/archive-detail.component';
+import { ArchiveListComponent } from './archive-list/archive-list.component';
+import { InsertArchiveComponent } from './insert-archive/insert-archive.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: ArchiveListComponent },
+  { path: 'create-archive', component: InsertArchiveComponent },
+  { path: 'archive/:id', component: ArchiveDetailComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

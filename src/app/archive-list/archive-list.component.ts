af5840860db9d2110e3a76939ce64153aea646cb
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-22 15:48:50 +0100

    Creata semplice lista per gli archivi e interfaccia

--- src/app/archive-list/archive-list.component.ts
+++ src/app/archive-list/archive-list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Archive{
+  id: number;
+  name: string;
+  path: string;
+}
+
 @Component({
   selector: 'app-archive-list',
   templateUrl: './archive-list.component.html',
   styleUrls: ['./archive-list.component.css']
 })
 export class ArchiveListComponent implements OnInit {
+
+  public archives: Array<Archive> = [{ id: 1, name: 'prova', path: 'prova'}];
 
   constructor() { }
 

7eae79118253ddaf00ddd55c239c423642b0fffe
Luca Biasiolo <luca.biasiolo@akera.it>
2021-02-23 10:24:55 +0100

    Creata cartella core con servizio e interfacce

---
+++ src/app/core/multimedia-archive-fe.interfaces.ts
@@ -0,0 +1,5 @@
+export interface IArchive {
+  id: number;
+  name: string;
+  path: string;
+}
---
+++ src/app/core/services/archive.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { IArchive } from '../multimedia-archive-fe.interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArchiveService {
+
+  private archivesArray: Array<IArchive> = [{ id: 1, name: 'prova', path: 'prova'}];
+
+  constructor() { }
+
+  public get archives(): Array<IArchive> {
+    return this.archivesArray;
+  }
+
+}
